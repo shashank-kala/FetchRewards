c328df0a8615deb038c309ed6bbb88c85e03de21
shashank-kala <65907700+shashank-kala@users.noreply.github.com>
2022-06-06 23:19:00 -0400

    Changes

--- Basic Data Quality.py
+++ Basic Data Quality.py
@@ -1,51 +1,58 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[2]:
 
 
 import pandas as pd;
 
 
-# In[8]:
+# In[3]:
 
 
 #Read Receipts Json File
 df = pd.read_json('receipts.json',lines = True)
 
 
-# In[9]:
+# In[4]:
 
 
 #Check for values in the head, Gives an overall look and feel of the data
 df.head()
 
 
-# In[12]:
+# In[5]:
 
 
 df.count()
 
 
-# In[14]:
+# In[6]:
 
 
 #Checking for the column name we can see it consists of multiple values 
 df.rewardsReceiptStatus.unique()
 
 
-# In[18]:
+# In[7]:
 
 
 # Check data type of CreateDate field , it contains object type ; Relational Model will contain Date type in the different fields
 df.createDate.dtypes
 
 
-# In[19]:
+# In[8]:
 
 
 # Th
 df.userId.unique()
+
+
+# In[18]:
+
+
+#Missing Values in the Datasets
+df.isna().sum()
 
 
 # In[ ]:
